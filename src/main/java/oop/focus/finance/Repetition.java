6bc08ad5b3f5e7f8445163f123ca15b8b527a68c
Alex Siroli <alex.siroli@studio.unibo.it>
2021-03-06 19:26:01 +0100

    aggiunta enum ripetizioni

---
+++ src/main/java/oop/focus/finance/Repetition.java
@@ -0,0 +1,34 @@
+package oop.focus.finance;
+
+public enum Repetition {
+
+    /**
+     * it is not repeated.
+     */
+    ONCE,
+    /**
+     * it is repeated every week.
+     */
+    WEEKLY,
+    /**
+     * it is repeated every month.
+     */
+    MONTHLY,
+    /**
+     * it is repeated every two months.
+     */
+    BIMONTHLY,
+    /**
+     * it is repeated every three months.
+     */
+    QUARTERLY,
+    /**
+     * it is repeated every six months.
+     */
+    HALF_YEARLY,
+    /**
+     * it is repeated every year.
+     */
+    YEARLY;
+
+}
