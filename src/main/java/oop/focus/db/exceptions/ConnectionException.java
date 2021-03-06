a8c41e030f4df89e1610faf410a84d1b97410154
Marco <marco.frattarola@studio.unibo.it>
2021-03-06 19:28:49 +0100

    aggiunte interfacce Connector, Dao e classi ConnectionException, DaoAccessException

---
+++ src/main/java/oop/focus/db/exceptions/ConnectionException.java
@@ -0,0 +1,7 @@
+package oop.focus.db.exceptions;
+
+/**
+ * This exception is thrown when ia not possible to establish a connection with the data source.
+ */
+public class ConnectionException extends Exception {
+}
---
+++ src/main/java/oop/focus/db/exceptions/DaoAccessException.java
@@ -0,0 +1,8 @@
+package oop.focus.db.exceptions;
+
+/**
+ * This exception is thrown when the data source cannot be accessed
+ * due to a specific exception of the Dao implementation.
+ */
+public class DaoAccessException extends Exception {
+}
