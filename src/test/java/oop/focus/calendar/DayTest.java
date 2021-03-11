1e0e5995b1b60914a67934fcd48574ff1e295af7
Edoardo.Puce <edoardo.puce@studio.unibo.it>
2021-03-11 16:12:18 +0100

    risolti warnings DayTest

--- src/test/java/oop/focus/calendar/DayTest.java
+++ src/test/java/oop/focus/calendar/DayTest.java
@@ -6,27 +6,21 @@ package oop.focus.calendar;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import org.joda.time.LocalDate;
-import org.joda.time.LocalDateTime;
 import org.junit.Before;
 import org.junit.Test;
-
-import oop.focus.homepage.model.Event;
-import oop.focus.homepage.model.EventImpl;
-import oop.focus.homepage.model.ManagerEventImpl;
-import oop.focus.homepage.model.Repetition;
 
 
 public class DayTest {
 
     private DayImpl day;
     private static final int YEAR = 2021;
-    private final ManagerEventImpl manager = new ManagerEventImpl();
+    //private final ManagerEventImpl manager = new ManagerEventImpl();
 
     @Before
     public void initDay() {
 
         final LocalDate date;
-        date = new LocalDate(YEAR, 3, 4);
+        date = new LocalDate(YEAR, 3, 26);
         day = new DayImpl(date);
 
     }
@@ -44,17 +38,6 @@ public class DayTest {
     
     @Test
     public void testEvent() {
-    	
-        final Event first = new EventImpl("Shopping", new LocalDateTime(2021, 9, 26, 9, 30), new LocalDateTime(2021, 9, 26, 10, 30), Repetition.NEVER);
-        final Event second = new EventImpl("Palestra", new LocalDateTime(2021, 9, 25, 8, 30), new LocalDateTime(2021, 9, 25, 10, 30), Repetition.NEVER);
-        final Event third = new EventImpl("Università", new LocalDateTime(2021, 9, 26, 11, 30), new LocalDateTime(2021, 9, 26, 18, 30), Repetition.NEVER);
-        final Event four = new EventImpl("Cinema", new LocalDateTime(2021, 9, 26, 19, 30), new LocalDateTime(2021, 9, 26, 22, 45), Repetition.NEVER);
-        
-    	manager.addEvent(first);
-    	manager.addEvent(second);
-    	manager.addEvent(third);
-    	manager.addEvent(four);
-      
 
     }
     
