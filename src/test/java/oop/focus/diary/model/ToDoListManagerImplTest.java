1a7f9a284d3d9d009904ebd2a42838243d955aa7
Alice <alice.mastrilli@studio.unibo.it>
2021-03-13 21:03:54 +0100

    aggiunta gestione eccezione metodo test

--- src/test/java/oop/focus/diary/model/ToDoListManagerImplTest.java
+++ src/test/java/oop/focus/diary/model/ToDoListManagerImplTest.java
@@ -12,11 +12,11 @@ public class ToDoListManagerImplTest {
     @Test
     public void test() {
         final ToDoListManager tdlm = new ToDoListManagerImpl();
-        final ToDoActionImpl prova = new ToDoActionImpl("cucinare");
+        final ToDoActionImpl prova = new ToDoActionImpl("cucinare", false);
         tdlm.addAnnotation(prova);
-        final ToDoActionImpl prova2 = new ToDoActionImpl("portare fuori la spazzatura");
+        final ToDoActionImpl prova2 = new ToDoActionImpl("portare fuori la spazzatura", false);
         tdlm.addAnnotation(prova2);
-        final ToDoActionImpl prova3 = new ToDoActionImpl("studiare");
+        final ToDoActionImpl prova3 = new ToDoActionImpl("studiare", false);
         tdlm.addAnnotation(prova3);
         assertEquals(prova.getAnnotation(), "cucinare");
         assertFalse(prova2.isDone());
@@ -24,6 +24,13 @@ public class ToDoListManagerImplTest {
         assertTrue(prova2.isDone());
         tdlm.getAnnotations().forEach(s -> System.out.println(s.getAnnotation()));
         tdlm.removeAnnotation(prova2);
+        tdlm.getAnnotations().forEach(s -> System.out.println(s.getAnnotation()));
+    }
+    @Test (expected = IllegalArgumentException.class)
+    public void testException() {
+        final ToDoListManager tdlm = new ToDoListManagerImpl();
+        final ToDoActionImpl prova = new ToDoActionImpl("cucinarecucinarecucinarecucinarecucinarecucinarecucinare", false);
+        tdlm.addAnnotation(prova);
         tdlm.getAnnotations().forEach(s -> System.out.println(s.getAnnotation()));
     }
 
