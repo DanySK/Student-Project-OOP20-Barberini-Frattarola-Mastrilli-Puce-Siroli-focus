8a687f581c1ca12c2fc6ac9585a9b36277ab0b99
Alice <alice.mastrilli@studio.unibo.it>
2021-03-15 11:09:37 +0100

    aggiunto test per salvataggio pagine di diario in memoria

--- src/main/java/oop/focus/diary/model/DiarySingleDao.java
+++ src/main/java/oop/focus/diary/model/DiarySingleDao.java
@@ -1,6 +1,7 @@
 package oop.focus.diary.model;
 
 
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -19,15 +20,13 @@ import java.util.Optional;
 
 import oop.focus.db.SingleDao;
 import oop.focus.db.exceptions.ConnectionException;
-import oop.focus.db.exceptions.DaoAccessException;
 
 public class DiarySingleDao implements SingleDao<DiaryImpl> {
     private static final String SEP = File.separator;
-    private final List<DiaryImpl> list;
+    private List<DiaryImpl> list;
     private final DiaryConnector d;
 
     public DiarySingleDao() {
-        this.list = new ArrayList<>();
         this.d = new DiaryConnector();
         try {
             this.d.create();
@@ -37,9 +36,13 @@ public class DiarySingleDao implements SingleDao<DiaryImpl> {
     }
     @Override
     public final List<DiaryImpl> getAll() {
-        Arrays.stream(Objects.<File[]>requireNonNull(d.getConnection().listFiles())).forEach(elem -> {
+        this.list = new ArrayList<>();
+        Arrays.stream(Objects.requireNonNull(d.getConnection().listFiles())).forEach(elem -> {
             try {
-                this.list.add(new DiaryImpl(getReader(elem).readLine(), elem.getName()));
+                final DiaryImpl newD =  new DiaryImpl(getReader(elem).readLine(), elem.getName());
+                if (!this.list.contains(newD)) {
+                    this.list.add(newD);
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -47,49 +50,44 @@ public class DiarySingleDao implements SingleDao<DiaryImpl> {
         return this.list;
     }
     private BufferedReader getReader(final File elem) throws FileNotFoundException {
-        return new BufferedReader(new FileReader(elem.toPath().toFile()));
+        return new BufferedReader(new FileReader(elem));
     }
-
 
     private Path getDiaryPath(final DiaryImpl x) {
-        return Paths.get(this.d.getConnection().getPath() + SEP + x.getName() + ".txt");
+        return Paths.get(this.d.getConnection().getPath() + SEP + x.getName());
     }
     @Override
-    public final void save(final DiaryImpl x) throws DaoAccessException {
+    public final void save(final DiaryImpl x) {
         final Path path = getDiaryPath(x);
-        try {
-            if (!Files.exists(path)) {
+        if (!Files.exists(path)) {
+            try {
                 Files.createFile(path);
-                this.list.add(x);
+                this.getAll().add(x);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
-
         try {
-            getDos(path).writeChars(x.getContent());
-
+            (getDos(path)).writeChars(x.getContent());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
     private DataOutputStream getDos(final Path path) throws FileNotFoundException {
         return new DataOutputStream(new FileOutputStream(path.toFile()));
     }
-
     @Override
-    public final void update(final DiaryImpl x) throws IllegalArgumentException, DaoAccessException {
-        final Optional<DiaryImpl> di = Optional.ofNullable(list.stream().filter(s -> Objects.equals(s.getName(), x.getName())).iterator().next());
-        if (di.isPresent()) {
-            di.get().setContent(x.getContent());
-            save(di.get());
+    public final void update(final DiaryImpl x) throws IllegalArgumentException {
+         for (final var elem : this.getAll()) {
+            if (elem.getName().equals(x.getName())) {
+                this.list.remove(this.getId(elem).get());
+                save(x);
+            }
         }
     }
 
     @Override
-    public final void delete(final DiaryImpl x) throws DaoAccessException {
+    public final void delete(final DiaryImpl x) {
         if (getAll().contains(x)) {
             this.list.remove(x);
             System.out.println(getDiaryPath(x).toFile().delete());
@@ -103,16 +101,16 @@ public class DiarySingleDao implements SingleDao<DiaryImpl> {
     }
     @Override
     public final Optional<DiaryImpl> getValue(final int id) {
-        if (list.get(id) != null) {
-            return Optional.of(list.get(id));
+              if (getAll().get(id) != null) {
+            return Optional.of(getAll().get(id));
         }
         return Optional.empty();
     }
 
     @Override
     public final Optional<Integer> getId(final DiaryImpl x) {
-        if (this.list.contains(x)) {
-            return Optional.of(this.list.indexOf(x));
+          if (this.getAll().contains(x)) {
+            return Optional.of(this.getAll().indexOf(x));
         }
         return Optional.empty();
     }
