56537a2de50bb645a7d043bc9695037f83c8885e
Alex Siroli <alex.siroli@studio.unibo.it>
2021-03-23 21:30:40 +0100

    aggiunti 10 colori e 53 categorie al database

--- src/main/resources/scripts/populate_script.sql
+++ src/main/resources/scripts/populate_script.sql
@@ -1,6 +1,65 @@
 insert into PUBLIC.COLOR (ID, VALUE)
-values  (1, '356734'),
-        (2, '123456');
+values  (1, 'ffffff'), --bianco
+        (2, '000000'); --nero
+        (3, 'ff0000'); --rosso
+        (4, 'ff6600'); --arancione
+        (5, 'ffff00'); --giallo
+        (6, '66ff66'); --verde
+        (7, '00ccff'); --azzurro
+        (8, '0000ff'); --blu
+        (9, 'cc00cc'); --viola
+        (10, '663300'); --marrone
 insert into PUBLIC.CATEGORY (NAME, ID_COLOR)
 values  ('Bar & locali', 1),
         ('Ristoranti & Fast-Food', 2);
+        ('Spesa', 3),
+        ('Abbigliamento & Scarpe', 4),
+        ('Animali domestici', 5),
+        ('Bellezza & Benessere', 6),
+        ('Cancelleria & Utensili', 7),
+        ('Casa & Giardino', 8),
+        ('Elettronica', 9),
+        ('Farmacia', 10),
+        ('Figli', 11),
+        ('Gioielli & Accessori', 12),
+        ('Regali', 13),
+        ('Tempo libero', 14),
+        ('Taxi', 15),
+        ('Trasporto pubblico', 16),
+        ('Viaggi', 17),
+        ('Assicurazione veicoli', 18),
+        ('Carburante', 19),
+        ('Leasing', 20),
+        ('Manutenzione veicoli', 21),
+        ('Noleggi', 22),
+        ('Parcheggio', 23),
+        ('Alcolici & Tabacco', 24),
+        ('Cultura & Eventi sportivi', 25),
+        ('Donazioni', 26),
+        ('Formazione & Sviluppo', 27),
+        ('Hobby', 28),
+        ('Libri', 29),
+        ('Lotterie & Giochi d\'azzardo', 30),
+        ('Salute', 31),
+        ('Sport & Fitness', 32),
+        ('TV & Streaming', 33),
+        ('Hotel & Alberghi', 34),
+        ('Internet', 35),
+        ('Servizi postali', 36),
+        ('Software, App & Giochi', 37),
+        ('Telefono', 38),
+        ('Assegno familiare', 39),
+        ('Assicurazioni', 40),
+        ('Consulenza', 41),
+        ('Multe', 42),
+        ('Prestiti', 43),
+        ('Tasse', 44),
+        ('Investimenti finanziari', 45),
+        ('Riscossioni', 46),
+        ('Buoni & Coupon', 47),
+        ('Dividendi', 48),
+        ('Quote & Contributi', 49),
+        ('Rimborsi', 50),
+        ('Stipendio', 51),
+        ('Vendite', 52),
+        ('Altro', 53);
