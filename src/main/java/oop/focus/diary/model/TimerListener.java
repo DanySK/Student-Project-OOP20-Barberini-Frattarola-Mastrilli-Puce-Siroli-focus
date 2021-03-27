9409aeb3a3e9475cad84cb72cd8152200f48ad55
Alice <alice.mastrilli@studio.unibo.it>
2021-03-27 16:56:01 +0100

    sistemato import TimerListener

--- src/main/java/oop/focus/diary/model/TimerListener.java
+++ src/main/java/oop/focus/diary/model/TimerListener.java
@@ -12,10 +12,15 @@ public interface TimerListener {
     void createEvent(Integer counter);
 
     /**
+     * Used when a counter starts, in order to register that time.
      * @throws  IllegalStateException if the counter couldn't start now, because another event is taking place
      * @return  true if the counter can start
-     * Used when a counter starts, in order to register that time.
      */
     boolean startCounter();
 
-    }
+    /**
+     * Used to signal that counter's value is changed.
+     * @param value the new value of counter
+     */
+    void counterValue(int value);
+}
