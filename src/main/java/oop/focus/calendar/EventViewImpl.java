bb221055c96c5587ea3283013619d1a76fb35b32
Edoardo.Puce <edoardo.puce@studio.unibo.it>
2021-03-27 12:42:23 +0100

    modifica import classe

--- src/main/java/oop/focus/calendar/EventViewImpl.java
+++ src/main/java/oop/focus/calendar/EventViewImpl.java
@@ -17,7 +17,7 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import oop.focus.finance.Repetition;
+import oop.focus.finance.model.Repetition;
 import oop.focus.homepage.model.Event;
 import oop.focus.homepage.model.EventImpl;
 
