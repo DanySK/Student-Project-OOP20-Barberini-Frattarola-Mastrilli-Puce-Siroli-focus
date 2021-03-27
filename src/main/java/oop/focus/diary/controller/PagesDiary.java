aa8a378a14f8df179cf2b7962450aa764f22ccf1
Alice <alice.mastrilli@studio.unibo.it>
2021-03-28 00:16:55 +0100

    aggiunta classe e interfaccia al controller per gestione diario

---
+++ src/main/java/oop/focus/diary/controller/PagesDiary.java
@@ -0,0 +1,50 @@
+package oop.focus.diary.controller;
+
+import javafx.collections.ObservableList;
+import oop.focus.diary.model.DiaryImpl;
+
+import java.util.List;
+
+/**
+ * This interface models a controller of pages' diary, saved as a file.
+ */
+public interface PagesDiary {
+    /**
+     * The method returns a list with all files' names.
+     * @return  a list of all files saved
+     */
+    List<String> filesName();
+
+    /**
+     * The list return an observable list with all diary saved(a diary is formed by title and content).
+     * @return  an observable list of all Diary's saved
+     */
+    ObservableList<DiaryImpl> getObservableList();
+
+    /**
+     * Return the content of page's diary whose title is the input's string.
+     * @param fileName  the title of a specific diary's page
+     * @return  the content of page's diary
+     */
+    String getContentByName(String fileName);
+
+    /**
+     * The method updates the diary's page whose title is "name", replacing his content with the input's "content"
+     * @param name  the name of diary's page
+     * @param content   the new content of diary's page
+     */
+    void updatePage(String name, String content);
+
+    /**
+     * The method removes the diary's page whose title is the string in input
+     * @param name  the title of page to remove
+     */
+    void removePage(String name);
+
+    /**
+     * The method creates a new diary's page whose name and content are specified
+     * @param name  the title of the page to insert
+     * @param content   the content of the page to insert
+     */
+    void createPage(String name, String content);
+}
---
+++ src/main/java/oop/focus/diary/controller/PagesDiaryImpl.java
@@ -0,0 +1,49 @@
+package oop.focus.diary.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javafx.collections.ObservableList;
+import oop.focus.diary.model.DiaryDao;
+import oop.focus.diary.model.DiaryImpl;
+
+public class PagesDiaryImpl implements PagesDiary {
+    private final DiaryDao diaryDao;
+    public PagesDiaryImpl() {
+        this.diaryDao = new DiaryDao();
+    }
+    @Override
+    public List<String> filesName() {
+        return this.diaryDao.getAll().stream().map(DiaryImpl::getName).collect(Collectors.toList());
+    }
+    @Override
+    public ObservableList<DiaryImpl> getObservableList() {
+         return this.diaryDao.getAll();
+     }
+    @Override
+    public String getContentByName(final String fileName) {
+        if (this.diaryDao.getAll().stream().anyMatch(s -> s.getName().equals(fileName))) {
+            return this.diaryDao.getAll().stream().filter(s -> s.getName().equals(fileName)).findAny().get().getContent();
+        }
+        throw new IllegalArgumentException();
+    }
+
+    @Override
+    public void updatePage(final String name, final String content) {
+        if (this.filesName().contains(name)) {
+            this.diaryDao.update(new DiaryImpl(content, name));
+            System.out.println(content);
+        }
+    }
+    @Override
+    public void removePage(final String name) {
+        if (this.filesName().contains(name)) {
+            this.diaryDao.delete(new DiaryImpl(this.getContentByName(name), name));
+        }
+    }
+    @Override
+    public void createPage(final String name, final String content) {
+        this.diaryDao.save(new DiaryImpl(content, name));
+    }
+
+}
