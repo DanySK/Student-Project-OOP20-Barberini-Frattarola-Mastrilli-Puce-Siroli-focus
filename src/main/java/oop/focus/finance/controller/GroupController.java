e70eb31f87832f969aa097ccccf61dbf47829031
Alex Siroli <alex.siroli@studio.unibo.it>
2021-04-06 01:23:45 +0200

    implementazione metodi del controller delle transazioni di gruppo

--- src/main/java/oop/focus/finance/controller/GroupController.java
+++ src/main/java/oop/focus/finance/controller/GroupController.java
@@ -1,4 +1,33 @@
 package oop.focus.finance.controller;
 
+import oop.focus.finance.model.GroupTransaction;
+import oop.focus.homepage.model.Person;
+
 public interface GroupController extends FinanceController {
+
+    /**
+     * Show people of group in view.
+     */
+    void showPeople();
+
+    /**
+     * Show group transactions in view.
+     */
+    void showTansactions();
+
+    /**
+     * @param person to be deleted
+     */
+    void deletePerson(Person person);
+
+    /**
+     * @param person whose credits I want to know
+     * @return returns person's credits
+     */
+    int getCredit(Person person);
+
+    /**
+     * @param transaction to be deleted
+     */
+    void deleteTransaction(GroupTransaction transaction);
 }
--- src/main/java/oop/focus/finance/controller/GroupControllerImpl.java
+++ src/main/java/oop/focus/finance/controller/GroupControllerImpl.java
@@ -1,8 +1,10 @@
 package oop.focus.finance.controller;
 
 import oop.focus.finance.model.FinanceManager;
+import oop.focus.finance.model.GroupTransaction;
 import oop.focus.finance.view.GroupViewImpl;
 import oop.focus.finance.view.View;
+import oop.focus.homepage.model.Person;
 
 public class GroupControllerImpl implements GroupController {
 
@@ -12,10 +14,36 @@ public class GroupControllerImpl implements GroupController {
     public GroupControllerImpl(final FinanceManager manager) {
         this.manager = manager;
         this.view = new GroupViewImpl(this);
+        this.showPeople();
     }
 
     @Override
     public final View getView() {
         return this.view;
+    }
+
+    @Override
+    public final void showPeople() {
+        this.view.showPeople(this.manager.getGroupManager().getGroup());
+    }
+
+    @Override
+    public final void showTansactions() {
+        this.view.showTransactions(this.manager.getGroupManager().getTransactions());
+    }
+
+    @Override
+    public final void deletePerson(final Person person) {
+        this.manager.getGroupManager().removePerson(person);
+    }
+
+    @Override
+    public final int getCredit(final Person person) {
+        return this.manager.getGroupManager().getCredit(person);
+    }
+
+    @Override
+    public final void deleteTransaction(final GroupTransaction transaction) {
+        this.manager.getGroupManager().removeTransaction(transaction);
     }
 }
