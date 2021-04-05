f4cf601591cee9d282f04664a773d71c236587a7
Alex Siroli <alex.siroli@studio.unibo.it>
2021-04-06 01:30:06 +0200

    implementazione della visualizzazione delle persone e delle transazioni di gruppo nella view del gruppo

--- src/main/java/oop/focus/finance/view/GroupView.java
+++ src/main/java/oop/focus/finance/view/GroupView.java
@@ -1,4 +1,22 @@
 package oop.focus.finance.view;
 
+import javafx.collections.ObservableSet;
+import oop.focus.finance.model.GroupTransaction;
+import oop.focus.homepage.model.Person;
+
 public interface GroupView extends View {
+
+    /**
+     * Show people in the groupMovementsScroll.
+     *
+     * @param group to be showed
+     */
+    void showPeople(ObservableSet<Person> group);
+
+    /**
+     * Show group transactions in the groupMovementsScroll.
+     *
+     * @param transactions to be showed
+     */
+    void showTransactions(ObservableSet<GroupTransaction> transactions);
 }
--- src/main/java/oop/focus/finance/view/GroupViewImpl.java
+++ src/main/java/oop/focus/finance/view/GroupViewImpl.java
@@ -1,16 +1,23 @@
 package oop.focus.finance.view;
 
+import javafx.collections.ObservableSet;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import oop.focus.finance.controller.FXMLPaths;
 import oop.focus.finance.controller.GroupController;
+import oop.focus.finance.model.GroupTransaction;
+import oop.focus.homepage.model.Person;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class GroupViewImpl implements Initializable, GroupView {
@@ -53,6 +60,29 @@ public class GroupViewImpl implements Initializable, GroupView {
     }
 
     private void populate() {
-        //devo dare ad ogni pulsante il proprio metodo del controller
+        this.peopleButton.setOnAction(event -> this.controller.showPeople());
+        this.groupTransactionsButton.setOnAction(event -> this.controller.showTansactions());
+    }
+
+    @Override
+    public final void showPeople(final ObservableSet<Person> group) {
+        List<PersonView> personTiles = new ArrayList<>();
+        group.forEach(p -> personTiles.add(new PersonViewImpl(this.controller, p)));
+        VBox box = new VBox();
+        personTiles.forEach(t -> box.getChildren().add(t.getRoot()));
+        personTiles.forEach(t -> t.getRoot()
+                .addEventHandler(MouseEvent.MOUSE_CLICKED, event -> this.controller.deletePerson(t.getPerson())));
+        this.groupMovementsScroll.setContent(box);
+    }
+
+    @Override
+    public final void showTransactions(final ObservableSet<GroupTransaction> transactions) {
+        List<GroupTransactionView> transactionTiles = new ArrayList<>();
+        transactions.forEach(t -> transactionTiles.add(new GroupTransactionViewImpl(this.controller, t)));
+        VBox box = new VBox();
+        transactionTiles.forEach(t -> box.getChildren().add(t.getRoot()));
+        transactionTiles.forEach(t -> t.getRoot()
+                .addEventHandler(MouseEvent.MOUSE_CLICKED, event -> this.controller.deleteTransaction(t.getTransaction())));
+        this.groupMovementsScroll.setContent(box);
     }
 }
