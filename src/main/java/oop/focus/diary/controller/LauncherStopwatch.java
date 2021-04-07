2657487501857e130958607aa527cf121331521b
Alice <alice.mastrilli@studio.unibo.it>
2021-04-07 13:31:19 +0200

    aggiunto auto dimensionamento della finestra del cronometro

--- src/main/java/oop/focus/diary/controller/LauncherStopwatch.java
+++ src/main/java/oop/focus/diary/controller/LauncherStopwatch.java
@@ -1,25 +1,24 @@
 package oop.focus.diary.controller;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Dimension2D;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import oop.focus.diary.view.StopwatchView;
 
 public class LauncherStopwatch extends Application {
     private static final String PATH_MAIN_STYLE = "/layouts/diary/diaryStyle.css";
-    private static final String PATH_BASE_DIARY = "/layouts/diary/stopwatchScheme.fxml";
-    private final Stage firstStage = new Stage();
-
 
     @Override
     public final void start(final Stage primaryStage) throws Exception {
-        final Parent root = FXMLLoader.load(this.getClass().getResource(PATH_BASE_DIARY));
-        final Scene scene = new Scene(root);
+        Dimension2D dim = new Dimension2D(1400, 900);
+        Scene scene = new Scene(new StopwatchView().getRoot());
+        primaryStage.setScene(scene);
         final String css = LauncherDiary.class.getResource(PATH_MAIN_STYLE).toExternalForm();
         scene.getStylesheets().add(css);
         primaryStage.setScene(scene);
-        this.firstStage.setResizable(true);
+        primaryStage.setWidth(dim.getWidth());
+        primaryStage.setHeight(dim.getHeight());
         primaryStage.show();
     }
     public static void main(final String[] args) {
--- src/main/java/oop/focus/diary/view/StopwatchView.java
+++ src/main/java/oop/focus/diary/view/StopwatchView.java
@@ -1,24 +1,44 @@
 package oop.focus.diary.view;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import oop.focus.diary.controller.ControllerFactoryImpl;
 import oop.focus.diary.controller.ControllersFactory;
 import oop.focus.diary.controller.CounterControllerImpl;
+import oop.focus.diary.controller.FXMLPaths;
 import oop.focus.diary.controller.UpdateView;
 import oop.focus.diary.controller.UseTotalTimeController;
 import org.joda.time.LocalTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class StopwatchView implements  Initializable {
     private static final  DateTimeFormatter TIME_FORMATTER = DateTimeFormat.forPattern("HH : mm : ss");
+    private static final double INSETS = 20;
+    private static final double H_GAP_PERCENTAGE = 0.07;
+    private static final double V_GAP_PERCENTAGE = 0.2;
+    private static final double LABEL_HEIGHT_PERCENTAGE = 0.15;
+    private static final double LABEL_WIDTH_PERCENTAGE = 0.25;
+    private static final double COMBO_BOX_HEIGHT = 0.1;
+    private static final double COMBO_BOX_WIDTH = 0.15;
+    private static final double ADD_EVENT_BUTTON_DIM = 0.05;
+    private static final double BUTTON_WIDTH_PERCENTAGE = 0.2;
+    @FXML
+    private Pane pane;
 
     @FXML
     private Label nameEventLabel;
@@ -40,7 +60,51 @@ public class StopwatchView implements  Initializable {
 
     @FXML
     private Button addNewEvent;
-
+    private Parent root;
+    public StopwatchView() {
+        final FXMLLoader loader = new FXMLLoader(this.getClass().getResource(FXMLPaths.STOPWATCH.getPath()));
+        loader.setController(this);
+        try {
+            this.root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.setProperties();
+    }
+    private void setProperties() {
+        final GridPane grid = new GridPane();
+        grid.addRow(0, this.nameEventLabel, this.chooseEvent, this.addNewEvent, this.timeLabel);
+        grid.add(this.counterLabel, (grid.getColumnCount() - 2) / 2, 1, 2, 1);
+        grid.add(this.startButton, 1, 2, 2, 1);
+        grid.add(this.stopButton, 2, 2, 2, 1);
+        grid.hgapProperty().bind(this.pane.widthProperty().multiply(H_GAP_PERCENTAGE));
+        grid.vgapProperty().bind(this.pane.heightProperty().multiply(V_GAP_PERCENTAGE));
+        this.pane.getChildren().add(grid);
+        GridPane.setHalignment(this.counterLabel, HPos.CENTER);
+        GridPane.setHalignment(this.nameEventLabel, HPos.CENTER);
+        this.setDimLabel(List.of(this.counterLabel, this.timeLabel, this.nameEventLabel));
+        this.setDimButton(List.of(this.startButton, this.stopButton));
+        this.chooseEvent.prefHeightProperty().bind(this.pane.heightProperty().multiply(COMBO_BOX_HEIGHT));
+        this.chooseEvent.prefWidthProperty().bind(this.pane.widthProperty().multiply(COMBO_BOX_WIDTH));
+        this.addNewEvent.prefHeightProperty().bind(this.pane.heightProperty().multiply(ADD_EVENT_BUTTON_DIM));
+        this.addNewEvent.prefWidthProperty().bind(this.pane.widthProperty().multiply(ADD_EVENT_BUTTON_DIM));
+        grid.setPadding(new Insets(INSETS));
+    }
+    private void setDimLabel(final List<Label> node) {
+        node.forEach(s -> {
+            s.prefWidthProperty().bind(this.pane.widthProperty().multiply(LABEL_WIDTH_PERCENTAGE));
+            s.prefHeightProperty().bind(this.pane.heightProperty().multiply(LABEL_HEIGHT_PERCENTAGE));
+        });
+    }
+    private void setDimButton(final List<Button> button) {
+        button.forEach(s -> {
+            s.prefWidthProperty().bind(this.pane.widthProperty().multiply(BUTTON_WIDTH_PERCENTAGE));
+            s.prefHeightProperty().bind(this.pane.heightProperty().multiply(LABEL_HEIGHT_PERCENTAGE));
+        });
+    }
+    public final Parent getRoot() {
+        return this.root;
+    }
     public final void initialize(final URL location, final ResourceBundle resources) {
         final ControllersFactory factory = new ControllerFactoryImpl();
         final CounterControllerImpl specificController = factory.createStopwatch();
