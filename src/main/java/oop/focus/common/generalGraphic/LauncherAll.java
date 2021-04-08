3a378250b3193bf6bf15806443a6e21168847528
Alice <alice.mastrilli@studio.unibo.it>
2021-04-08 22:04:21 +0200

    modifica LauncherAll

--- src/main/java/oop/focus/common/generalGraphic/LauncherAll.java
+++ src/main/java/oop/focus/common/generalGraphic/LauncherAll.java
@@ -1,23 +1,18 @@
 package oop.focus.common.generalGraphic;
 
 import javafx.application.Application;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.Window;
-import oop.focus.diary.controller.LauncherDiary;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class LauncherAll extends Application {
     private static final String PATH_MAIN_STYLE = "/layouts/generalGraphics/GeneralStyle.css";
     private final CommonView commonView = new CommonView(640,480);
     @Override
     public void start(Stage primaryStage) throws Exception {
-        final String css = LauncherDiary.class.getResource(PATH_MAIN_STYLE).toExternalForm();
+        //final String css = LauncherDiary.class.getResource(PATH_MAIN_STYLE).toExternalForm();
         Scene scene = new Scene(commonView.getPane());
-        scene.getStylesheets().add(css);
+        //scene.getStylesheets().add(css);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
