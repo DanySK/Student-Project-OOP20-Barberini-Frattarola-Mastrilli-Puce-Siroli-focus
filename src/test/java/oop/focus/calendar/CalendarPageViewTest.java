aa766c72681af8f0c696a2bb36a6df8a6a277f08
Edoardo.Puce <edoardo.puce@studio.unibo.it>
2021-04-09 12:45:23 +0200

    aggiunte classi per test view calendario

---
+++ src/test/java/oop/focus/calendar/CalendarPageViewTest.java
@@ -0,0 +1,30 @@
+package oop.focus.calendar;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import oop.focus.calendar.controller.CalendarController;
+import oop.focus.calendar.controller.CalendarControllerImpl;
+
+
+
+
+public class CalendarPageViewTest extends Application {
+
+    private static final double WIDTH = 700;
+    private static final double HEIGHT = 500;
+
+    public final void start(final Stage primaryStage) {
+
+        final Pane generalpage = new Pane();
+        final CalendarController pageview = new CalendarControllerImpl(generalpage, 300, 150, 200, 500);
+        primaryStage.setScene(new Scene(pageview.getCalendarPage(), WIDTH, HEIGHT));
+        primaryStage.show();
+    }
+
+    public static final void main(final String... args) {
+        launch(args);
+    }
+}
+
