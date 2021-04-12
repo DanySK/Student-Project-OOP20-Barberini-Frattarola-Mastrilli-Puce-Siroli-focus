5250588775f0ffc27b44470a9c6e8d8017772de9
Alice <alice.mastrilli@studio.unibo.it>
2021-04-12 13:35:06 +0200

    risolti errori in diaryDaoTest

--- src/test/java/oop/focus/diary/model/DiaryDaoTest.java
+++ src/test/java/oop/focus/diary/model/DiaryDaoTest.java
@@ -1,49 +1,43 @@
 package oop.focus.diary.model;
-
-import oop.focus.db.exceptions.DaoAccessException;
 import org.junit.Test;
 
 public class DiaryDaoTest {
     private final DiaryDao dsd = new DiaryDao();
-    private final DiaryImpl diario1 = new DiaryImpl("oggi il tempo è brutto, sta per piovere", "oggi");
-    private final DiaryImpl diario2 = new DiaryImpl("domani il tempo sarà bello e il sole splenderà", "domani");
+    private final DiaryImpl diario1 = new DiaryImpl("oggi il tempo è brutto, sta per piovere", "oggioggi");
+    private final DiaryImpl diario2 = new DiaryImpl("domani il tempo sarà bello e il sole splenderà", "domanidomani");
+    private final DiaryImpl diario3 = new DiaryImpl("provaprovaprova", "provaprova");
+    private final DiaryImpl diario4 = new DiaryImpl("ciaociao", "provaprova");
+    private final DiaryImpl diario5 = new DiaryImpl("aaaaa", "prova1prova1");
+    private final DiaryImpl diario6 = new DiaryImpl("ccccc", "provaprova2");
+
     @Test
-    public void testBackupPages() throws DaoAccessException {
+    public void testBackupPages() {
         this.dsd.save(this.diario1);
         this.dsd.save(this.diario2);
         this.dsd.getAll().forEach(a -> System.out.println(a.getName() + " " + a.getContent()));
-    }    @Test
-    public void testUpdate() throws DaoAccessException {
-        this.dsd.getAll().forEach(a -> System.out.println(a.getName() + " " + a.getContent()));
-        this.dsd.update(new DiaryImpl("ha smesso di piovere", "oggi"));
-        this.dsd.getAll().forEach(a -> System.out.println(a.getName() + " " + a.getContent()));
-        this.dsd.update(new DiaryImpl("ha smesso di piovere2", "ieri"));
-        this.dsd.getAll().forEach(a -> System.out.println(a.getName() + " " + a.getContent()));
-        this.dsd.delete(this.dsd.getAll().stream().filter(a -> a.equals(diario1)).findAny().get());
-        System.out.println("dopo eliminazione : " );
-        this.dsd.getAll().forEach(a -> System.out.println(a.getName() + " "+ a.getContent()));
-
-
-
+        this.dsd.delete(this.diario1);
+        this.dsd.delete(this.diario2);
     }
-    
+    @Test
+    public void testUpdate() {
+        this.dsd.save(this.diario5);
+        this.dsd.getAll().forEach(a -> System.out.println(a.getName() + " " + a.getContent()));
+        this.dsd.update(new DiaryImpl("bbbbb", "prova1"));
+        this.dsd.getAll().forEach(a -> System.out.println(a.getName() + " " + a.getContent()));
+        this.dsd.delete(this.diario5);
+    }
+    @Test (expected = IllegalArgumentException.class)
+    public void testUpdateNewNote() {
+        this.dsd.update(diario6);
+        this.dsd.getAll().forEach(a -> System.out.println(a.getName() + " " + a.getContent()));
+    }
     @Test
     public void anotherTest() {
-
-        this.dsd.save(new DiaryImpl("pro", "proa"));
+        this.dsd.save(this.diario3);
         this.dsd.getAll().forEach(a -> System.out.println(a.getName() + " "+ a.getContent()));
-        this.dsd.update(new DiaryImpl("ciaoooo", "proa"));
+        this.dsd.update(this.diario4);
         this.dsd.getAll().forEach(a -> System.out.println(a.getName() + " "+ a.getContent()));
-        this.dsd.save(new DiaryImpl("pro", "proa"));
+        this.dsd.delete(this.diario4);
     }
-    @Test
-    public void test2() {
-        this.dsd.getAll().forEach(a -> System.out.println(a.getName() + " "+ a.getContent()));
-    }
-
-
- 
-
-
 
 }
