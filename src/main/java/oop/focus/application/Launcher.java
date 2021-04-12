63829c980e676d2f992abeec7e2e708d4739b717
Alice <alice.mastrilli@studio.unibo.it>
2021-04-12 19:22:22 +0200

    rinominate classi App e Launcher

---
+++ src/main/java/oop/focus/application/Launcher.java
@@ -0,0 +1,10 @@
+package oop.focus.application;
+
+
+public final  class Launcher {
+    private Launcher() {
+}
+    public static void main(final String... args) {
+        App.main(args);
+    }
+}
