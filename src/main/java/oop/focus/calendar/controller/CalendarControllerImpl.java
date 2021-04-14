09fa99a0b5a3a314f535fcd1917d01d6b2f539fb
Edoardo.Puce <edoardo.puce@studio.unibo.it>
2021-04-14 23:32:19 +0200

    piccola modifica

--- src/main/java/oop/focus/calendar/controller/CalendarControllerImpl.java
+++ src/main/java/oop/focus/calendar/controller/CalendarControllerImpl.java
@@ -38,7 +38,7 @@ public class CalendarControllerImpl implements CalendarController {
      */
     public CalendarControllerImpl(final DataSource dataSource) {
 
-        this.monthController = new CalendarMonthControllerImpl(CalendarType.DIARY, dataSource);
+        this.monthController = new CalendarMonthControllerImpl(CalendarType.NORMAL, dataSource);
 
         settingsController = new CalendarSettingsControllerImpl(monthController);
 
