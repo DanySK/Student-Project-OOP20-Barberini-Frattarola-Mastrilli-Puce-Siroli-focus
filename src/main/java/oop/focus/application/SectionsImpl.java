af5e9b96cb935ccadcd780732183bf92c4fbf9e3
Alice <alice.mastrilli@studio.unibo.it>
2021-04-15 22:34:29 +0200

    aggiunto home page in grafica generale

--- src/main/java/oop/focus/application/SectionsImpl.java
+++ src/main/java/oop/focus/application/SectionsImpl.java
@@ -7,20 +7,26 @@ import oop.focus.db.DataSourceImpl;
 
 import oop.focus.diary.controller.GeneralDiaryController;
 import oop.focus.finance.controller.BaseControllerImpl;
+import oop.focus.finance.model.FinanceManager;
 import oop.focus.finance.model.FinanceManagerImpl;
+import oop.focus.homepage.view.GeneralHomePageController;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class SectionsImpl implements Sections {
     private final List<Pair<Controller, String>> list;
     private final DataSource dataSource;
+    private final FinanceManager financeManager;
     public SectionsImpl() {
         this.dataSource = new DataSourceImpl();
+        this.financeManager = new FinanceManagerImpl(this.dataSource);
         this.list = new ArrayList<>();
         this.putControllers();
     }
     private void putControllers() {
-        this.list.add(new Pair<>(new BaseControllerImpl(new FinanceManagerImpl(this.dataSource)), "Finanza"));
+        this.list.add(new Pair<>(new GeneralHomePageController(this.dataSource, this.financeManager), "Home Page"));
+        this.list.add(new Pair<>(new BaseControllerImpl(this.financeManager), "Finanza"));
         this.list.add(new Pair<>(new CalendarControllerImpl(this.dataSource), "Calendario"));
         this.list.add(new Pair<>(new GeneralDiaryController(this.dataSource), "Diario"));
     }
