d7443b593acc4eb7dfac8b93d61b4191839e5ffc
Elisa Barberini <elisabarberini0025@gmail.com>
2021-04-22 18:28:54 +0200

    sistemazione tasto contatore

--- src/main/java/oop/focus/calendarhomepage/view/CounterHotKeyView.java
+++ src/main/java/oop/focus/calendarhomepage/view/CounterHotKeyView.java
@@ -3,11 +3,9 @@ package oop.focus.calendarhomepage.view;
 import oop.focus.calendarhomepage.model.HotKeyImpl;
 import oop.focus.calendarhomepage.model.HotKeyType;
 import org.joda.time.LocalDateTime;
-
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.HBox;
 import oop.focus.common.Repetition;
 import oop.focus.calendarhomepage.controller.HomePageController;
 import oop.focus.event.model.EventImpl;
@@ -15,7 +13,7 @@ import oop.focus.event.model.EventImpl;
 /**
  * This class is used to model an event hot key, wich is represented from a button, and a label.
  */
-public class CounterHotKeyView extends Pane implements HotKeyView {
+public class CounterHotKeyView extends HBox implements HotKeyView {
 
     private final HomePageController controller;
     private final Button button;
@@ -25,12 +23,10 @@ public class CounterHotKeyView extends Pane implements HotKeyView {
         this.controller = controller;
         this.label = new Label();
         this.button = new Button(buttonName);
-        this.button.setStyle("-fx-background-color:" + HotKeyType.COUNTER.getColor() + ";");
         this.setAction();
         this.label.setText(this.controller.getClickTime(new HotKeyImpl(this.button.getText(), HotKeyType.COUNTER)));
-        this.label.setAlignment(Pos.TOP_LEFT);
-        this.button.setAlignment(Pos.TOP_RIGHT);
         this.getChildren().addAll(this.button, this.label);
+        this.setStyle("-fx-background-color:" + HotKeyType.COUNTER.getColor() + ";");
     }
 
     public final String getLabelValue() {
