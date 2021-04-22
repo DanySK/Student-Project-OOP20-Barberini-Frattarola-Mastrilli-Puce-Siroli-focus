8c190613548b4420057cd243060def56885ed946
Alice Mastrilli <alice.mastrilli@studio.unibo.it>
2021-04-22 22:24:56 +0200

    modifica DailyMoodTest

--- src/test/java/oop/focus/diary/model/DailyMoodManagerImplTest.java
+++ src/test/java/oop/focus/diary/model/DailyMoodManagerImplTest.java
@@ -9,15 +9,17 @@ public class DailyMoodManagerImplTest {
     private final DailyMoodManagerImpl dailyMoodManager = new DailyMoodManagerImpl(new DataSourceImpl());
     @Test
     public void testDailyMood() throws DaoAccessException {
-        final DailyMood mood1 =new DailyMoodImpl(2, LocalDate.now());
-        final DailyMood mood2 = new DailyMoodImpl(3, LocalDate.now());
-        final DailyMood mood3 = new DailyMoodImpl(4, LocalDate.now().plusDays(4));
+        final LocalDate date = new LocalDate(20, 10, 10);
+        final LocalDate date1 = new LocalDate(20,10,11);
+        final DailyMood mood1 = new DailyMoodImpl(2, date);
+        final DailyMood mood2 = new DailyMoodImpl(3, date);
+        final DailyMood mood3 = new DailyMoodImpl(4, date1);
         this.dailyMoodManager.addDailyMood(mood1);
-        assertEquals(java.util.Optional.of(2), this.dailyMoodManager.getMoodByDate(LocalDate.now()));
+        assertEquals(java.util.Optional.of(2), this.dailyMoodManager.getMoodByDate(date));
         this.dailyMoodManager.modifyDailyMood(mood2);
-        assertEquals(java.util.Optional.of(3), this.dailyMoodManager.getMoodByDate(LocalDate.now()));
+        assertEquals(java.util.Optional.of(3), this.dailyMoodManager.getMoodByDate(date));
         this.dailyMoodManager.addDailyMood(mood3);
-        assertEquals(java.util.Optional.of(4), this.dailyMoodManager.getMoodByDate(LocalDate.now().plusDays(4)));
+        assertEquals(java.util.Optional.of(4), this.dailyMoodManager.getMoodByDate(date1));
         this.dailyMoodManager.deleteDailyMood(mood3);
         this.dailyMoodManager.deleteDailyMood(mood1);
         this.dailyMoodManager.deleteDailyMood(mood2);
